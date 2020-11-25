aa040fecba758a90bb42b09455fe46a5edf87d2b
czaorz <czasg0.0>
2020-11-25 21:21:48 +0800

    feat: update 初版

--- putest/__init__.py
+++ putest/__init__.py
@@ -79,8 +79,8 @@ import loggus
 from .{'moduleName'} import *
 """
 funcTemplate = f"""
-def UnitTest_{'funcName'}():
-    entry = logger.withField("funcName", "add")
+def UnitTest_{'funcName'}(log: loggus.Entry):
+    entry = log.withField("funcName", "add")
 
     samples = [
         {{
@@ -117,8 +117,80 @@ def UnitTest_{'funcName'}():
 """
 
 
-def create():
-    pass
+def create(py: str):
+    py = py.replace(".py", "")
+    try:
+        module = importlib.import_module(py)
+    except:
+        loggus.withTraceback().panic("ImportModuleErr")
+        return
+    template = f"""# coding: utf-8
+import loggus
+
+from .{py} import *\n
+"""
+    for attr in dir(module):
+        if attr.startswith(("_", "UnitTest")):
+            continue
+        attrIns = getattr(module, attr)
+        if not inspect.isfunction(attrIns):
+            continue
+        sig = inspect.signature(attrIns)
+        parameters = []
+        argsKeys = []
+        argsValues = []
+        for k, v in sig.parameters.items():
+            argsKeys.append(k)
+            if v.kind == v.POSITIONAL_ONLY:
+                argsValues.append(k)
+            elif v.kind == v.POSITIONAL_OR_KEYWORD:
+                argsValues.append(k)
+            elif v.kind == v.VAR_POSITIONAL:
+                argsValues.append(f"*{k}")
+            elif v.kind == v.KEYWORD_ONLY:
+                argsValues.append(f"**{k}")
+            elif v.kind == v.VAR_KEYWORD:
+                argsValues.append(f"**{k}")
+            parameters.append("                {},".format({k: {"kind": v.kind.__str__()}}))
+        argsKeys = ", ".join(argsKeys)
+        if argsKeys:
+            argsKeys += " = sample[\"parameters\"]"
+        argsValues = ", ".join(argsValues)
+        parameters = "\n".join(parameters)
+        template += f"""
+def UnitTest_{attr}(log: loggus.Entry):
+    entry = log.withField("funcName", "{attr}")
+
+    samples = [
+        {{
+            "name": str,
+            "parameters": [
+{parameters}
+            ],
+            "want": None,
+            "wantErr": bool,
+        }},
+    ]
+    for sample in samples[1:]:
+        log = entry.withField("sampleName", sample["name"])
+        want = None
+        try:
+            {argsKeys}
+            want = {attr}({argsValues})
+        except:
+            if not sample["wantErr"]:
+                log.withTraceback().error("ExceptionErr")
+                continue
+        if want != sample["want"]:
+            log.withFields({{
+                "want": sample["want"],
+                "actual": want,
+            }}).error("EqualErr")
+            continue
+        log.info("pass")\n
+"""
+    with open(f"{py}_test.py", "w", encoding="utf-8") as f:
+        f.write(template)
 
 
 def scan():
@@ -141,5 +213,20 @@ def scan():
     collector.show()
 
 
+def execute():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd", nargs="*", default=[])
+    args = parser.parse_args()
+    if len(args.cmd) == 0:
+        return
+    if args.cmd[0] == "test":
+        scan()
+    elif args.cmd[0] == "create" and len(args.cmd) > 1:
+        create(args.cmd[1])
+
+
 if __name__ == '__main__':
-    scan()
+    # scan()
+    create("temp.py")
