d4cb2b853f79dfde44aa6011d43047702cd32149
陈子昂 <chenziang@antiy.cn>
2020-11-25 18:53:17 +0800

    feat: update 未完成版本

--- putest/__init__.py
+++ putest/__init__.py
@@ -1,11 +1,83 @@
 # coding: utf-8
 import os
+import sys
 import loggus
 import inspect
+import colorama
 import importlib
 
 from datetime import datetime
+from threading import RLock
 
+
+class Collector:
+    allSample = 0
+    passSample = 0
+    failSample = 0
+    lock = RLock()
+
+    def add(self):
+        with self.lock:
+            self.allSample += 1
+
+    def addPass(self):
+        with self.lock:
+            self.passSample += 1
+            self.add()
+
+    def addFail(self):
+        with self.lock:
+            self.failSample += 1
+            self.add()
+
+    def show(self):
+        print("\r\n --------------- \r\n")
+        print(f"用例总量: {self.allSample}")
+        print(f"成功:{self.passSample}")
+        print(f"失败:{self.failSample}")
+        if self.failSample:
+            print(colorama.Fore.RED + f"测试不通过")
+            sys.exit(50)
+        else:
+            print(colorama.Fore.GREEN + f"测试通过")
+            sys.exit(0)
+
+
+collector = Collector()
+
+
+class PUTLogger(loggus.Logger):
+    pass
+
+
+class CollectorInfoHook(loggus.IHook):
+
+    def GetLevels(self):
+        return [loggus.INFO]
+
+    def ProcessMsg(self, msg):
+        collector.addPass()
+
+
+class CollectorErrorHook(loggus.IHook):
+
+    def GetLevels(self):
+        return [loggus.ERROR]
+
+    def ProcessMsg(self, msg):
+        collector.addFail()
+
+
+logger = loggus.NewLogger()
+logger.AddHook(CollectorInfoHook())
+logger.AddHook(CollectorErrorHook())
+entry = loggus.NewEntry(logger)
+
+template = f"""
+import loggus
+
+from .{'moduleName'} import *
+"""
 funcTemplate = f"""
 def UnitTest_{'funcName'}():
     entry = logger.withField("funcName", "add")
@@ -45,29 +117,29 @@ def UnitTest_{'funcName'}():
 """
 
 
-def add(x, y):
+def create():
     pass
 
 
-def warning():
-    pass
-
-
-if __name__ == '__main__':
-    import os
-    import importlib
-
+def scan():
     for path, dirs, files in os.walk("."):
         for file in files:
             if not file.endswith("_test.py"):
                 continue
-            module = os.path.join(path, file).replace(".\\", "").replace("\\", ".").replace(".py", "")
+            module = os.path.join(path, file). \
+                replace(".\\", ""). \
+                replace("\\", "."). \
+                replace(".py", "")
             try:
+                log = entry.withField("module", module)
                 module = importlib.import_module(module)
                 for attr in dir(module):
                     if attr.startswith("UnitTest_"):
-                        getattr(module, attr)()
+                        getattr(module, attr)(log)
             except:
-                import traceback
+                entry.withTraceback().panic("TestErr")
+    collector.show()
 
-                print(traceback.format_exc())
+
+if __name__ == '__main__':
+    scan()
